6ae541bcbbdfe361fcd34c930b1582cdec45d503
ddmtchr <rmr04489@gmail.com>
2023-05-22 22:36:46 +0300

    Fixed multithreading

--- server/src/main/java/server/RequestThread.java
+++ server/src/main/java/server/RequestThread.java
@@ -5,7 +5,6 @@ import utility.Request;
 import utility.Response;
 import utility.ResponseBuilder;
 
-import java.net.SocketAddress;
 import java.util.concurrent.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,65 +12,40 @@ import java.util.logging.Logger;
 public class RequestThread implements Runnable {
     private static final Logger logger = Logger.getLogger(Server.class.getName());
     private ConnectionProvider connectionProvider;
-    private RequestProcessor requestProcessor;
-    private final ExecutorService requestReceivingExecutor;
-    private final ExecutorService requestProcessingExecutor;
-    private final ForkJoinPool responseSendingExecutor;
-    private final BlockingQueue<Request> requestQueue;
-    private final BlockingQueue<Response> responseQueue;
+    private ExecutorService receivingThread;
+    private ExecutorService processingThread;
+    private ForkJoinPool forkJoinPool;
 
     public RequestThread(ConnectionProvider connectionProvider) {
-        requestReceivingExecutor = Executors.newSingleThreadExecutor();
-        requestProcessingExecutor = Executors.newSingleThreadExecutor();
-        responseSendingExecutor = ForkJoinPool.commonPool();
-        requestQueue = new LinkedBlockingQueue<>();
-        responseQueue = new LinkedBlockingQueue<>();
+        receivingThread = Executors.newSingleThreadExecutor();
+        processingThread = Executors.newSingleThreadExecutor();
+        forkJoinPool = ForkJoinPool.commonPool();
         this.connectionProvider = connectionProvider;
     }
 
+    @Override
     public void run() {
         while (true) {
             try {
-                putRequestOnQueue();
-                SocketAddress addr = takeRequestAndProcess();
-                takeResponseAndSend(addr);
+                Future<Request> requestFuture = receivingThread.submit(connectionProvider::receive);
+                Request request = requestFuture.get();
+                if (request != null) {
+                    CompletableFuture
+                            .supplyAsync(() -> request)
+                            .thenApplyAsync(receivedRequest -> {
+                                RequestProcessor requestProcessor = new RequestProcessor(
+                                        receivedRequest.getUserName(), receivedRequest.getUserPassword());
+                                int execCode = requestProcessor.processRequest(receivedRequest);
+                                if (execCode == 0) logger.log(Level.INFO, "Запрос выполнен");
+                                else logger.log(Level.WARNING, "Ошибка выполнения запроса");
+                                requestProcessor.closeDBConnection();
+                                return new Response(ResponseBuilder.getAndClear(), execCode, receivedRequest.getHost());
+                            }, processingThread)
+                            .thenAcceptAsync(response -> connectionProvider.send(response), forkJoinPool);
+                }
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
         }
-    }
-
-    private void putRequestOnQueue() throws InterruptedException, ExecutionException {
-        Future<Request> waitingFuture = requestReceivingExecutor.submit(connectionProvider::receive);
-        Request clientRequest = waitingFuture.get();
-        if (clientRequest != null) {
-            requestQueue.put(clientRequest);
-            logger.log(Level.INFO, "Запрос принят");
-        }
-    }
-
-    private SocketAddress takeRequestAndProcess() throws InterruptedException, ExecutionException {
-        Request clientRequest = requestQueue.take();
-        requestProcessor = new RequestProcessor(clientRequest.getUserName(), clientRequest.getUserPassword());
-        Future<Response> processingFuture = requestProcessingExecutor.submit(
-                () -> {
-                    int execCode = requestProcessor.processRequest(clientRequest);
-                    if (execCode == 0) logger.log(Level.INFO, "Запрос выполнен");
-                    else logger.log(Level.WARNING, "Ошибка выполнения запроса");
-                    return new Response(ResponseBuilder.getAndClear(), execCode);
-                });
-        Response serverResponse = processingFuture.get();
-        if (serverResponse != null) {
-            responseQueue.put(serverResponse);
-            return clientRequest.getHost();
-        }
-        return null;
-    }
-
-    private void takeResponseAndSend(SocketAddress clientHost) throws InterruptedException {
-        Response serverResponse = responseQueue.take();
-        Future<?> sendingFuture = responseSendingExecutor.submit(() ->
-                connectionProvider.send(serverResponse, clientHost));
-        logger.log(Level.INFO, "Ответ отправлен");
     }
 }
